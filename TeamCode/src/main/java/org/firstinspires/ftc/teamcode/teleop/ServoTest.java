5435579423ad839d291bbc5902452bb9ecf3a804
ProNoob1029 <pronoob1029@gmail.com>
2022-02-21 14:46:24 +0200

    servo test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ServoTest.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(group = "drive")
+public class ServoTest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Servo servo = hardwareMap.get(Servo.class, "servo");
+
+        waitForStart();
+
+        while (!isStopRequested()) {
+
+            servo.setPosition(gamepad1.right_trigger);
+
+            telemetry.addData("right trigger", gamepad1.right_trigger);
+            telemetry.update();
+        }
+    }
+}
